14d56eeac110cea438fbaa4fae5cba46a9649d79
BoomBakar <abubakardar608@gmail.com>
2022-05-24 18:19:25 +0500

    Array List initialized

--- app/src/main/java/com/example/adapter/MainActivity.java
+++ app/src/main/java/com/example/adapter/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> Friends = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Friends.add("Adil");
+        Friends.add("Anns");
+        Friends.add("Usman");
+        Friends.add("Ahmad");
+        Friends.add("Saad");
+
+
+
+
+
     }
 }
